4c5f45ea7f4d5815306e030fa8c7ddb5caee6309
Christian Fischer <christian@chfi.se>
2022-09-25 12:49:54 -0500

    create node definitions

--- src/graph.rs
+++ src/graph.rs
@@ -1,9 +1,8 @@
-
 use rustc_hash::FxHashMap;
 use wgpu::*;
 
-use std::sync::Arc;
-
+use anyhow::Result;
+use std::{collections::HashMap, sync::Arc};
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
 pub struct TextureResourceSig {
@@ -16,7 +15,6 @@ pub struct TextureResource {
     texture: wgpu::Texture,
     layout: wgpu::ImageDataLayout,
 }
-
 
 pub struct RenderGraph {
     images: FxHashMap<usize, ()>,
@@ -40,8 +38,33 @@ pub enum SocketDir {
 
 #[derive(Clone)]
 pub struct NodeDef {
+    name: String,
     input_sockets: Vec<(String, DataType)>,
     output_sockets: Vec<(String, DataType)>,
+}
+
+impl NodeDef {
+    pub fn new<'a, 'b>(
+        name: &str,
+        inputs: impl IntoIterator<Item = (&'a str, DataType)>,
+        outputs: impl IntoIterator<Item = (&'b str, DataType)>,
+    ) -> Self {
+        let input_sockets = inputs
+            .into_iter()
+            .map(|(n, d)| (n.to_string(), d))
+            .collect();
+
+        let output_sockets = outputs
+            .into_iter()
+            .map(|(n, d)| (n.to_string(), d))
+            .collect();
+
+        Self {
+            name: name.to_string(),
+            input_sockets,
+            output_sockets,
+        }
+    }
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -58,3 +81,21 @@ pub struct Node {
     outputs: Vec<Option<(NodeId, SocketIx)>>,
 }
 
+pub struct Graph {
+    defs: HashMap<String, Arc<NodeDef>>,
+    nodes: Vec<Node>,
+}
+
+impl Graph {
+    pub fn init() -> Result<Self> {
+        let mut defs = HashMap::new();
+
+        let alloc_img_def = NodeDef::new("create_image", [], [("image", DataType::Image)]);
+
+        defs.insert(alloc_img_def.name.clone(), Arc::new(alloc_img_def));
+
+        let nodes = Vec::new();
+
+        Ok(Self { defs, nodes })
+    }
+}
