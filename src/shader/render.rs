79331a68045f9f3c358c05a470d207c0d565a22f
Christian Fischer <christian@chfi.se>
2022-10-16 16:19:40 -0500

    *basically* creating vertex state from shader source

--- src/shader/render.rs
+++ src/shader/render.rs
@@ -22,6 +22,14 @@ impl VertexInput {
     pub fn size(&self) -> u64 {
         self.format.size()
     }
+
+    pub fn attribute(&self, offset: u64) -> wgpu::VertexAttribute {
+        wgpu::VertexAttribute {
+            format: self.format,
+            offset,
+            shader_location: self.location
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -38,6 +46,44 @@ pub struct VertexShader {
 }
 
 impl VertexShader {
+    /* 
+    /// Creates a `wgpu::VertexState` that assumes all the vertex
+    /// attributes are stored tightly packed in a single buffer,
+    /// in `location` order.
+    pub fn into_vertex_state<'a>(
+        &'a self,
+        step_mode: wgpu::VertexStepMode,
+        //
+    ) -> Result<wgpu::VertexState<'a>> {
+
+        let mut attributes = Vec::new();
+        let mut array_stride = 0;
+
+        for input in self.vertex_inputs.iter() {
+            let attr = wgpu::VertexAttribute {
+                format: input.format,
+                offset: array_stride,
+                shader_location: input.location,
+            };
+            attributes.push(attr);
+            array_stride += input.format.size();
+        }
+
+        let layout = wgpu::VertexBufferLayout {
+            array_stride,
+            step_mode,
+            attributes: attributes.as_slice(),
+        };
+
+        let vx_state = wgpu::VertexState {
+            module: &self.shader_module,
+            entry_point: &self.entry_point.name,
+            buffers: &[layout],
+        };
+
+        Ok(vx_state)
+    }
+    */
 
     pub fn from_spirv(
         state: &crate::State,
@@ -109,7 +155,9 @@ impl VertexShader {
                     }
                 };
 
-                if let Some(format) = naga_to_wgpu_vertex_format(size, kind, width) {
+                if let Some(format) =
+                    naga_to_wgpu_vertex_format(size, kind, width)
+                {
                     let input = VertexInput {
                         name: arg.name.clone().unwrap_or_default(),
                         location,
@@ -123,7 +171,7 @@ impl VertexShader {
         vertex_inputs.sort_by_key(|v| v.location);
 
         log::warn!("vertex inputs: {:#?}", vertex_inputs);
-        
+
         let bind_group_layouts =
             GroupBindings::create_bind_group_layouts_checked(
                 &group_bindings,
@@ -146,8 +194,8 @@ fn naga_to_wgpu_vertex_format(
     kind: naga::ScalarKind,
     width: u8,
 ) -> Option<wgpu::VertexFormat> {
-    use naga::VectorSize as Size;
     use naga::ScalarKind as Kind;
+    use naga::VectorSize as Size;
     use wgpu::VertexFormat as Vx;
     match (vec_size, kind, width) {
         (None, Kind::Float, 4) => Some(Vx::Float32),
