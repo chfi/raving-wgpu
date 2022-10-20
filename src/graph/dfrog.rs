3566a2bc9d580f37aac7dd912bfcc406e16d84ec
Christian Fischer <christian@chfi.se>
2022-10-20 18:33:37 -0500

    (WIP) graph validation

--- src/graph/dfrog.rs
+++ src/graph/dfrog.rs
@@ -1,10 +1,14 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use datafrog::{Iteration, Relation, RelationLeaper, Variable};
 use rustc_hash::FxHashMap;
-use wgpu::TextureUsages;
+use wgpu::{SubmissionIndex, TextureUsages};
 
-use crate::{texture::Texture, ComputeShaderOp, DataType, GraphicsPipelineOp};
+use std::sync::Arc;
+
+use crate::{
+    texture::Texture, ComputeShaderOp, DataType, GraphicsPipelineOp, State,
+};
 
 use anyhow::Result;
 
@@ -22,9 +26,14 @@ pub struct NodeSchema {
     socket_names: Vec<rhai::ImmutableString>,
 
     source_sockets: Vec<(LocalSocketIx, DataType)>,
-    source_rules: Vec<(LocalSocketIx, SocketMetadataSource)>,
-
+    source_rules_sockets: Vec<(LocalSocketIx, SocketMetadataSource)>,
+    source_rules_scalars: Vec<(LocalSocketIx, rhai::ImmutableString)>,
+    // source_rules_scalars: Vec<(LocalSocketIx, Vec<rhai::ImmutableString>)>,
     default_sources: FxHashMap<LocalSocketIx, ResourceMeta>,
+
+    create_source_metadata: Option<
+        Arc<dyn Fn(&rhai::Map, Option<ResourceMeta>) -> Result<ResourceMeta>>,
+    >,
 }
 
 /*
@@ -38,9 +47,9 @@ fn create_image_schema(schema_id: NodeSchemaId) -> NodeSchema {
 
         socket_names,
         source_sockets,
-        source_rules: Relation::from_vec(Vec::new()),
+        source_rules_sockets: Relation::from_vec(Vec::new()),
 
-        default_sources: 
+        default_sources:
     }
 }
 */
@@ -74,7 +83,9 @@ pub enum NodeType {
 pub struct NodeSchemaSocketIx(usize);
 
 type LocalSocketIx = usize;
+
 type ResourceId = usize;
+type TransientId = usize;
 
 pub enum SocketState {
     Uninitialized,
@@ -90,7 +101,7 @@ pub enum NodeState {
 }
 */
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum ResourceMeta {
     Buffer {
         size: Option<usize>,
@@ -104,13 +115,31 @@ pub enum ResourceMeta {
     },
 }
 
+impl ResourceMeta {
+    pub fn buffer_default() -> Self {
+        Self::Buffer {
+            size: None,
+            usage: None,
+            stride: None,
+        }
+    }
+
+    pub fn texture_default() -> Self {
+        Self::Texture {
+            size: None,
+            format: None,
+            usage: None,
+        }
+    }
+}
+
 pub enum Resource {
     Buffer(wgpu::Buffer),
     Texture(Texture),
 }
 
 pub struct Node {
-    schema: Option<NodeSchemaId>,
+    schema: NodeSchemaId,
 
     links: Vec<(LocalSocketIx, (NodeId, LocalSocketIx))>,
 }
@@ -121,27 +150,30 @@ pub struct Graph {
     nodes: Vec<Node>,
     // nodes: Vec<NodeId>,
     node_names: FxHashMap<NodeId, rhai::ImmutableString>,
+
+    socket_resources: FxHashMap<(NodeId, LocalSocketIx), ResourceId>,
+
     // node_names: Vec<rhai::ImmutableString>,
-    resource_defs: Vec<ResourceMeta>,
+    resource_meta: Vec<ResourceMeta>,
     resources: Vec<Option<Resource>>,
 
-    // links: FxHashMap<NodeId, Vec<(usize, NodeId, usize)>>,
+    transients_meta_cache: HashMap<String, ResourceMeta>,
+}
 
-    // node_schema: Relation<(NodeId, NodeSchemaId)>,
+pub enum InputResource<'a> {
+    Texture {
+        size: [u32; 2],
+        format: wgpu::TextureFormat,
 
-    // node_schema: Vec<NodeSchema>,
-    // node_schema: Vec<NodeSchemaId>,
+        texture: Option<&'a wgpu::Texture>,
+        view: Option<&'a wgpu::TextureView>,
+    },
+    Buffer {
+        size: usize,
+        stride: Option<usize>,
 
-    // links: Relation<(NodeId, (LocalSocketIx, NodeId, LocalSocketIx))>,
-    // links: Relation<((NodeId, LocalSocketIx), (NodeId, LocalSocketIx))>,
-
-    // resource_meta: Vec<ResourceMeta>,
-    // resources: Vec<Option<Resource>>,
-    // socket_resources: Relation<((NodeId, LocalSocketIx), ResourceId)>,
-    // resources: Vec<()>,
-    // socket_resources: Relation<(NodeId, (LocalSocketIx, ResourceId))>,
-
-    // local_socket_names: Relation<
+        buffer: Option<&'a wgpu::Buffer>,
+    },
 }
 
 impl Graph {
@@ -151,12 +183,142 @@ impl Graph {
             nodes: Vec::new(),
             node_names: FxHashMap::default(),
 
-            resource_defs: Vec::new(),
+            resource_meta: Vec::new(),
             resources: Vec::new(),
+
+            socket_resources: FxHashMap::default(),
+            transients_meta_cache: HashMap::default(),
         }
     }
 
-    pub fn add_node(&mut self, schema: Option<NodeSchemaId>) -> NodeId {
+    pub fn validate<'a>(
+        &mut self,
+        transient_res: &'a HashMap<String, InputResource<'a>>,
+        graph_scalar_in: &'a rhai::Map,
+    ) -> Result<bool> {
+        /*
+            validate graph:
+             - create resource IDs for each resource origin/source in the graph
+             - make sure sockets are only used once (each for I/O)
+             - make sure all sockets end up with a valid resource ID (not unique)
+
+            also make sure the `resource_meta` vector is up to date,
+            i.e. each element should be fully filled in and be ready to be used
+            to allocate a new resource
+
+            likewise transients_meta_cache should be populated
+
+        */
+
+        /*
+            update transients cache
+        */
+
+        let mut invalidated_transients = Vec::new();
+
+        for (key, in_res) in transient_res.iter() {
+            let meta = match in_res {
+                InputResource::Texture {
+                    size,
+                    format,
+                    texture,
+                    view,
+                } => ResourceMeta::Texture {
+                    size: Some(*size),
+                    format: Some(*format),
+                    usage: None,
+                },
+                InputResource::Buffer {
+                    size,
+                    stride,
+                    buffer,
+                } => ResourceMeta::Buffer {
+                    size: Some(*size),
+                    usage: None,
+                    stride: *stride,
+                },
+            };
+
+            if let Some(cache) = self.transients_meta_cache.get_mut(key) {
+                if cache != &meta {
+                    *cache = meta;
+                    invalidated_transients.push(key);
+                }
+            } else {
+                self.transients_meta_cache.insert(key.to_string(), meta);
+                invalidated_transients.push(key);
+            }
+        }
+
+        for (ix, node) in self.nodes.iter().enumerate() {
+            let node_id = NodeId::from(ix);
+            let schema = &self.schemas[node.schema.0];
+
+            for &(socket_ix, ty) in schema.source_sockets.iter() {
+                let socket_key = (node_id, socket_ix);
+                if self.socket_resources.contains_key(&socket_key) {
+                    continue;
+                }
+
+                let res_id = self.resource_meta.len();
+
+                let meta = match ty {
+                    DataType::Buffer => ResourceMeta::buffer_default(),
+                    DataType::Texture => ResourceMeta::texture_default(),
+                };
+                self.resource_meta.push(meta);
+                self.resources.push(None);
+
+                self.socket_resources.insert(socket_key, res_id);
+            }
+        }
+
+        // now, build the relations for the datafrog programs
+
+        // node(node_id, schema_id).
+        let nodes = Relation::from_iter(
+            self.nodes
+                .iter()
+                .enumerate()
+                .map(|(ix, node)| (NodeId::from(ix), node.schema)),
+        );
+
+        // resource ID for each socket
+        // socket_resource(socket_ix, res_id)
+        let socket_resources = Relation::from_iter(
+            self.socket_resources.iter().map(|(&k, &v)| (k, v)),
+        );
+
+
+        /*
+          goal: match each socket up with a `resource_origin`
+         */
+
+
+
+
+        /*
+          with all sockets known to point to valide resource IDs, we can now
+          attempt to allocate the corresponding resources
+
+          (this is where NodeScheme's create_source_metadata comes in)
+         */
+
+
+        Ok(true)
+    }
+
+    pub fn execute<'a>(
+        &mut self,
+        state: &State,
+        transient_res: &'a HashMap<String, InputResource<'a>>,
+        graph_scalar_in: &'a rhai::Map,
+        // node_scalar_in: FxHashMap<NodeId, rhai::Map>,
+    ) -> Result<SubmissionIndex> {
+        todo!();
+    }
+
+    pub fn add_node(&mut self, schema: NodeSchemaId) -> NodeId {
         let id = NodeId::from(self.nodes.len());
 
         let node = Node {
@@ -176,11 +338,9 @@ impl Graph {
         dst: NodeId,
         dst_socket: LocalSocketIx,
     ) {
-
         let node = &mut self.nodes[src.0];
         node.links.push((src_socket, (dst, dst_socket)));
     }
-
 
     /*
     pub fn resolve_sockets(&mut self) {
@@ -196,15 +356,13 @@ impl Graph {
         // let to_resolve = Relation::from_antijoin(input1, input2, logic)
     }
     */
-    
 
-    pub fn add_schemas(&mut self) -> Result<()>  {
-
-        let image_schema = {
+    pub fn add_schemas(&mut self) -> Result<()> {
+        {
             let id = NodeSchemaId(self.schemas.len());
 
             let mut default_sources = FxHashMap::default();
-            
+
             // TODO these are temporary
             let size = [800, 600];
             let usage = TextureUsages::all();
@@ -218,6 +376,12 @@ impl Graph {
                 },
             );
 
+            let create_source_metadata = |scalars: &rhai::Map,
+                                          meta: Option<ResourceMeta>|
+             -> Result<ResourceMeta> {
+                todo!();
+            };
+
             let schema = NodeSchema {
                 node_type: NodeType::Resource,
                 schema_id: id,
@@ -225,16 +389,20 @@ impl Graph {
                 socket_names: vec!["image".into()],
 
                 source_sockets: vec![(0, DataType::Texture)],
-                source_rules: vec![],
+                source_rules_sockets: vec![],
+                source_rules_scalars: vec![(0, "dimensions".into())],
 
                 default_sources,
+
+                create_source_metadata: Some(Arc::new(create_source_metadata)),
             };
 
             self.schemas.push(schema);
             id
         };
 
-        let compute_node_schema = {
+        /*
+        {
             let id = NodeSchemaId(self.schemas.len());
 
             let schema = NodeSchema {
@@ -253,7 +421,7 @@ impl Graph {
                     (3, DataType::Buffer),
                 ],
 
-                source_rules: vec![
+                source_rules_sockets: vec![
                     (
                         1,
                         SocketMetadataSource {
@@ -270,12 +438,15 @@ impl Graph {
                     ),
                 ],
 
+                source_rules_scalars: vec![],
+
                 default_sources: FxHashMap::default(),
             };
 
             self.schemas.push(schema);
             id
         };
+        */
 
         Ok(())
     }
