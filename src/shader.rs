a2dfcd0ebe77569665753792366f8b14dedb3560
Christian Fischer <christian@chfi.se>
2022-10-30 11:10:08 -0500

    add ComputeShader::from_wgsl()

--- src/shader.rs
+++ src/shader.rs
@@ -54,6 +54,100 @@ impl ComputeShader {
         Ok(bind_groups)
     }
 
+    pub fn from_wgsl(
+        state: &super::State,
+        shader_src: &str,
+        entry_point: &str,
+    ) -> Result<Self> {
+
+        let label = format!("Compute shader");
+        let shader_desc = ShaderModuleDescriptor {
+            label: Some(label.as_str()),
+            source: wgpu::ShaderSource::Wgsl(shader_src.into()),
+        };
+        
+        let naga_mod = naga::front::wgsl::parse_str(shader_src)?;
+
+        let shader_module = state.device.create_shader_module(shader_desc);
+
+        
+        log::error!("parsing group bindings");
+        let group_bindings =
+            GroupBindings::from_spirv(&naga_mod, wgpu::ShaderStages::COMPUTE)?;
+        log::error!("group bindings: {:#?}", group_bindings);
+
+        let (workgroup_size, stage) = {
+            let entry_point = naga_mod
+                .entry_points
+                .iter()
+                .next()
+                .ok_or(anyhow!("shader entry point missing"))?;
+
+            (entry_point.workgroup_size, entry_point.stage)
+        };
+
+        let push_constants = naga_mod
+            .global_variables
+            .iter()
+            .find_map(|(_handle, var)| {
+                (var.space == naga::AddressSpace::PushConstant).then(|| {
+                    interface::PushConstants::from_naga_struct(
+                        &naga_mod,
+                        &naga_mod.types[var.ty].inner,
+                        stage,
+                    )
+                })
+            })
+            .transpose()?;
+
+        let bind_group_layouts =
+            GroupBindings::create_bind_group_layouts_checked(
+                &group_bindings,
+                state,
+            )?;
+
+        let layout_refs = bind_group_layouts.iter().collect::<Vec<_>>();
+
+        let push_constant_ranges = if let Some(p) = &push_constants {
+            dbg!();
+            vec![p.to_range(0, wgpu::ShaderStages::COMPUTE)]
+        } else {
+            dbg!();
+            vec![]
+        };
+
+        let pipeline_layout_desc = PipelineLayoutDescriptor {
+            label: None,
+            bind_group_layouts: layout_refs.as_slice(),
+            push_constant_ranges: push_constant_ranges.as_slice(),
+        };
+
+        let pipeline_layout =
+            state.device.create_pipeline_layout(&pipeline_layout_desc);
+
+        let compute_desc = ComputePipelineDescriptor {
+            label: None,
+            layout: Some(&pipeline_layout),
+            module: &shader_module,
+            entry_point,
+        };
+
+        let compute_pipeline =
+            state.device.create_compute_pipeline(&compute_desc);
+
+        let compute_shader = ComputeShader {
+            pipeline: compute_pipeline,
+
+            group_bindings,
+            bind_group_layouts,
+            workgroup_size,
+
+            push_constants,
+        };
+
+        Ok(compute_shader)
+    }
+
     pub fn from_spirv(
         state: &super::State,
         shader_src: &[u8],
